01eaea6f8c2fb2ec10663e729b555eda1bc73fbc
AliaMcAfee <mcafeealia@gmail.com>
2025-01-21 10:30:59 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <h1>React Weather Project</h1>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React Weather Project</h1>
+      <footer>
+        This project was coded by Alia McAfee open-sourced on{" "}
+        <a href="https://github.com/AliaMcAfee/react-weather-project.git">
+          Github
+        </a>{" "}
+        and hosted on <a href="">Netlify</a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
