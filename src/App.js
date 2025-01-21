64982e5a8769287990cc37aa08fe94068ced8a3d
AliaMcAfee <mcafeealia@gmail.com>
2025-01-21 17:40:11 -0600

    revised start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <h1>React Weather Project</h1>
       <Weather />
-      <footer>
-        This project was coded by Alia McAfee open-sourced on{" "}
-        <a href="https://github.com/AliaMcAfee/react-weather-project.git">
-          Github
-        </a>{" "}
-        and hosted on <a href="">Netlify</a>
-      </footer>
     </div>
   );
 }
