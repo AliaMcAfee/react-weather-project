cb48e5d941420c6b28b1c01ec40ce1ab1ae6ec9c
AliaMcAfee <mcafeealia@gmail.com>
2025-01-24 17:02:37 -0600

    Updated HTML

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ export default function App() {
     <div className="App">
       <h1>React Weather Project</h1>
       <Weather />
+      <p>
+        This project was coded by Alia McAfee and is open sourced on{" "}
+        <a href="https://github.com/AliaMcAfee/react-weather-project.git">
+          Github
+        </a>{" "}
+      </p>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,42 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1>Chicago</h1>
       <ul>
         <li>Wednesday 08:00</li>
         <li>Cloudy</li>
       </ul>
       <div className="row">
-        <div className="col-6">
+        <div className="col-3">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
             alt="Cloudy"
-          >
-            23°
-          </img>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 0% </li>
-              <li>Humidity: 56%</li>
-              <li>Wind: 10 mph</li>
-            </ul>
-          </div>
+          />
+          23°
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 56%</li>
+            <li>Wind: 10 mph</li>
+          </ul>
         </div>
       </div>
     </div>
