d4e7b777a2a3ef909002a8161dc582edba7a738a
AliaMcAfee <mcafeealia@gmail.com>
2025-01-26 20:05:26 -0600

    Updates to HTML

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather Project</h1>
-      <Weather />
-      <p>
-        This project was coded by Alia McAfee and is open sourced on{" "}
-        <a href="https://github.com/AliaMcAfee/react-weather-project.git">
-          Github
-        </a>{" "}
-      </p>
+      <div className="container">
+        <h1>React Weather Project</h1>
+        <Weather />
+        <p>
+          This project was coded by Alia McAfee and is open sourced on{" "}
+          <a href="https://github.com/AliaMcAfee/react-weather-project.git">
+            Github
+          </a>{" "}
+        </p>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -11,31 +11,39 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form"
-            />
+            ></input>
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary"
+            ></input>
           </div>
         </div>
       </form>
-      <h1>Chicago</h1>
-      <ul>
-        <li>Wednesday 08:00</li>
-        <li>Cloudy</li>
-      </ul>
       <div className="row">
-        <div className="col-3">
+        <div className="col-6">
+          <h1>Chicago</h1>
+          <ul>
+            <li>Sunday 08:00</li>
+            <li>Partly Cloudy</li>
+          </ul>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
           <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="Cloudy"
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partly Cloudy"
           />
-          23°
+          24°
         </div>
         <div className="col-6">
           <ul>
             <li>Precipitation: 0%</li>
-            <li>Humidity: 56%</li>
-            <li>Wind: 10 mph</li>
+            <li>Humidity: 53%</li>
+            <li>Wind: 7mph</li>
           </ul>
         </div>
       </div>
