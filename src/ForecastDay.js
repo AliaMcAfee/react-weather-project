d1b034e8c839c846553eb9ba4b7502e83258f9f6
AliaMcAfee <mcafeealia@gmail.com>
2025-02-03 12:00:06 -0600

    Converted days

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -2,17 +2,31 @@ import React from "react";
 import WeatherSvg from "./WeatherSvg";
 
 export default function ForecastDay(props) {
+  function temperatureMax() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}`;
+  }
+
+  function temperatureMin() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}`;
+  }
+
+  function day() {
+    let date = new Date(props.data.temperature.day * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div>
-      <div className="ForecastDay">{props.data.temperature.day}</div>
-      <WeatherSvg />
+      <div className="ForecastDay">{day()}</div>
+      <WeatherSvg code={props.data.icon} />
       <div className="ForecastTemperature">
-        <span className="temperature-max">
-          {props.data.temperature.maximum}°
-        </span>{" "}
-        <span className="temperature-min">
-          {props.data.temperature.minimum}°
-        </span>
+        <span className="temperature-max">{temperatureMax()}°</span>{" "}
+        <span className="temperature-min">{temperatureMin()}°</span>
       </div>
     </div>
   );
